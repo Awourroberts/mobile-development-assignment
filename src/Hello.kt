9792b97b913a94abdbdf2faa4895be9540376e0b
Awourrobberrt <sophierobberrt@gmail.com>
2021-02-20 22:31:42 +0300

    first commit

---
+++ src/Hello.kt
@@ -0,0 +1,21 @@
+fun main() {
+val fullName = "Sophie Robert"
+var age = 22
+var phoneNumber = "0769636921"
+var weight = 68
+val citizen = "kenyan"
+println(fullName)
+println(age)
+println(phoneNumber)
+println(weight)
+println(citizen)
+}
+
+
+
+
+
+
+
+
+
